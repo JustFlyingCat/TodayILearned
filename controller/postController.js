fe9bf0752b93ca0af67d159bd3e145883d348f98
Max Puchstein <maximilian.puchstein@sandstorm.de>
2020-07-15 09:37:29 +0200

    Linking post user

--- controller/postController.js
+++ controller/postController.js
@@ -20,12 +20,13 @@ exports.post = async function (req, res) {
     let title;
     let content;
     let date;
+    let userName;
 
     //Setting relations between the tables 
     User.hasMany(Post);
     Post.belongsTo(User);
 
-    const post = await Post.findOne({where: {id: req.params.postId, userId: req.params.userId}})
+    const post = await Post.findOne({where: {id: req.params.postId, userId: req.params.userId}, include: User});
 
     if (post == null) {
        title = '404 NOT FOUND';
@@ -33,8 +34,9 @@ exports.post = async function (req, res) {
     } else {
         title = post.headLine;
         date = post.createdAt;
+        userName = post.user.userName;
         content = post.content;
         //console.log(userPosts);
     }
-    res.render('post',{title: title, creationDate: date, postContent: content});
+    res.render('post',{title: title,user: userName, creationDate: date, postContent: content});
 }
