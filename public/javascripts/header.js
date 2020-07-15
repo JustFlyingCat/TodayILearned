c29e084a84b2608e5dd6ae0c4039dcafdd08172a
Max Puchstein <maximilian.puchstein@sandstorm.de>
2020-07-15 16:28:07 +0200

    new css style

---
+++ public/javascripts/header.js
@@ -0,0 +1,32 @@
+onload = function() {
+
+    const home = document.getElementById("Home");
+    const allUsers = document.getElementById("AllUsers");
+    const allPosts = document.getElementById("AllPosts");
+    const createPost = document.getElementById("CreatePost");
+    const title = document.title;
+
+    if (title == 'Today i learned') {
+        home.classList.add('active');
+    } else if(home.classList.contains('active')) {
+        home.classList.remove('active');
+    }
+
+    if (title == 'All Users') {
+        allUsers.classList.add('active');
+    } else if(allUsers.classList.contains('active')){
+        allUsers.classList.remove('active');
+    }
+
+    if (title == 'All Posts') {
+        allPosts.classList.add('active');
+    } else if(allPosts.classList.contains('active')){
+        allPosts.classList.remove('active');
+    }
+
+    if (title == 'Create a Post') {
+        createPost.classList.add('active');
+    } else if(createPost.classList.contains('active')){
+        createPost.classList.remove('active');
+    }
+}
